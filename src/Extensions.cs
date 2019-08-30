1b84cd762903f8f4da41c77a59eabd120901afa4
villainoustourist <villainoustourist@users.noreply.github.com>
2019-08-29 21:52:17 -0500

    Updates to .NET Core 3.0 preview 8 (and async)

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,18 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace BlazorPagination
 {
     public static class Extensions
     {
-        public static PagedResult<T> ToPagedResult<T>(this IQueryable<T> query, int page, int pageSize) where T : class
+        public async static Task<PagedResult<T>> ToPagedResult<T>(this IQueryable<T> query, int page, int pageSize) where T : class
         {
             page = page < 1 ? 1 : page;
             var result = new PagedResult<T> { CurrentPage = page, PageSize = pageSize, RowCount = query.Count() };
             var pageCount = (double)result.RowCount / pageSize;
             result.PageCount = (int)Math.Ceiling(pageCount);
             var skip = (page - 1) * pageSize;
-            result.Results = query.Skip(skip).Take(pageSize).ToList();
+            result.Results = await query.Skip(skip).Take(pageSize).ToArrayAsync();
             return result;
         }
     }
--- src/PagedResult.cs
+++ src/PagedResult.cs
@@ -4,11 +4,6 @@ namespace BlazorPagination
 {
     public class PagedResult<T> : PagedResultBase where T : class
     {
-        public IList<T> Results { get; set; }
-
-        public PagedResult()
-        {
-            Results = new List<T>();
-        }
+        public T[] Results { get; set; }
     }
 }
