3e4b3e5d3f9e57dc48fea712da9d592b79a33eb3
villainoustourist <villainoustourist@users.noreply.github.com>
2021-03-23 18:16:17 -0500

    try to recreate #5

--- example/Data/WeatherForecastService.cs
+++ example/Data/WeatherForecastService.cs
@@ -9,23 +9,14 @@ namespace BlazorPagerExample.Data
     public class WeatherForecastService
     {
         private static List<WeatherForecast> _summaries;
+        private static Random rand = new Random();
         public WeatherForecastService()
         {
-            _summaries = new List<WeatherForecast>
+            _summaries = new List<WeatherForecast>();
+            var date = DateTime.Now;
+            for (int i = 0; i < 141; i++)
             {
-                new WeatherForecast {Date = new DateTime(2020, 1, 1), Summary = "Freezing", TemperatureC = 2},
-                new WeatherForecast {Date = new DateTime(2020, 1, 2), Summary = "Cold", TemperatureC = 4},
-                new WeatherForecast {Date = new DateTime(2020, 1, 3), Summary = "Chilly", TemperatureC = 1},
-                new WeatherForecast {Date = new DateTime(2020, 1, 4), Summary = "Cold", TemperatureC = 6},
-                new WeatherForecast {Date = new DateTime(2020, 1, 5), Summary = "Bracing", TemperatureC = 7},
-                new WeatherForecast {Date = new DateTime(2020, 1, 6), Summary = "Chilly", TemperatureC = 10},
-                new WeatherForecast {Date = new DateTime(2020, 1, 7), Summary = "Mild", TemperatureC = 6},
-                new WeatherForecast {Date = new DateTime(2020, 1, 7), Summary = "Mild", TemperatureC = 6},
-                new WeatherForecast {Date = new DateTime(2020, 1, 8), Summary = "Cool", TemperatureC = 11},
-                new WeatherForecast {Date = new DateTime(2020, 1, 9), Summary = "Mild", TemperatureC = 14},
-                new WeatherForecast {Date = new DateTime(2020, 1, 10), Summary = "Freezing", TemperatureC = 17},
-                new WeatherForecast {Date = new DateTime(2020, 1, 11), Summary = "Freezing", TemperatureC = 19},
-                new WeatherForecast {Date = new DateTime(2020, 1, 12), Summary = "Freezing", TemperatureC = 13}
+                _summaries.Add(new WeatherForecast { Date = date.AddDays(i), Summary = "Freezing", TemperatureC = rand.Next(6, 22) });
             };
         }
 
